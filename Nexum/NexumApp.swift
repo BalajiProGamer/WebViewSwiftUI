549bd18cca2e4d22b0dc9dba2e75aae4eb216495
BalajiProGamer <balajibalamurugan1@gmail.com>
2025-02-19 11:02:37 +0530

    Initial Commit

---
+++ Nexum/NexumApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexumApp.swift
+//  Nexum
+//
+//  Created by Balaji Balamurugan on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct NexumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NexumTests/NexumTests.swift
@@ -0,0 +1,17 @@
+//
+//  NexumTests.swift
+//  NexumTests
+//
+//  Created by Balaji Balamurugan on 2/19/25.
+//
+
+import Testing
+@testable import Nexum
+
+struct NexumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
