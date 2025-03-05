1684bcf534f5e587cb1509ec5f82026d57cee03f
BalajiProGamer <balajibalamurugan1@gmail.com>
2025-03-06 03:39:03 +0530

    Final

--- Nexum/WebView.swift
+++ Nexum/WebView.swift
@@ -5,3 +5,18 @@
 //  Created by Balaji Balamurugan on 3/5/25.
 //
 
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
